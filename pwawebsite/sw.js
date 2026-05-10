387d362de56eaccaccccf9218987d53fe0563152
Cemal <cemal201138.dev@gmail.com>
2026-05-10 11:28:47 +0300

    Update service worker to version 2 and assets

--- pwawebsite/sw.js
+++ pwawebsite/sw.js
@@ -1,20 +1,25 @@
-const CACHE_NAME = 'cemal-evli-v1';
+const CACHE_NAME = 'cemal-evli-v2';
 const ASSETS = [
-  './',
-  './index.html',
-  './projects.json',
-  './games.json',
-  './files.json'
+  '/pwawebsite/',
+  '/pwawebsite/index.html',
+  '/pwawebsite/manifest.json',
+  '/pwawebsite/projects.json',
+  '/pwawebsite/games.json',
+  '/pwawebsite/files.json'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
   );
 });
